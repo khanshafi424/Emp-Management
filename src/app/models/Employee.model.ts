b62c2860f09f8bb0f91702f0f6dcf23818efc080
khanshafi424 <you@example.com>
2025-12-23 23:56:46 +0530

    created interface and class constructor for Employee

---
+++ src/app/models/Employee.model.ts
@@ -0,0 +1,50 @@
+export interface IEmployee {
+	empId: number;
+	empName: string;
+	contactNo: string;
+	email: string;
+	deptName: string;
+	designation: string;
+	createdDate: Date;
+	userName: string;
+	password: string;
+	sickLeaveBalance: number;
+	paidLeaveBalance: number;
+	role: string;
+}
+
+export class EmployeeModel {
+	empId: number;
+	empName: string;
+	contactNo: string;
+	email: string;
+	deptName: string;
+	designation: string;
+	createdDate: Date;
+	userName: string;
+	password: string;
+	sickLeaveBalance: number;
+	paidLeaveBalance: number;
+	role: string;
+
+	constructor() {
+		this.empId = 0;
+		this.empName =  '';
+		this.contactNo = '';
+		this.email = '';
+		this.deptName =  '';
+		this.designation ='';
+		this.createdDate = new Date();;
+		this.userName = '';
+		this.password = '';
+		this.sickLeaveBalance = 0;
+		this.paidLeaveBalance =  0;
+		this.role = '';
+	}
+
+}
+
+export interface IApiResponseModel{
+    message: string;
+    data:any;
+}
