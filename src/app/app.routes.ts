3ce9ef290fa24c60d6c52f28af9152e2e23ce89a
khanshafi424 <you@example.com>
2025-12-24 22:07:59 +0530

    created route for leave admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,7 +25,7 @@ export const routes: Routes = [
 
             },
             {
-                path:'balanec',
+                path:'balance',
                 component: LeaveBalance
 
             },
