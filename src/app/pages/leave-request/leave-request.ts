8854d41b54402aeb0df85de16051aecc67d2c422
khanshafi424 <you@example.com>
2025-12-24 22:16:49 +0530

    creating leave request  business logic for employee

---
+++ src/app/pages/leave-request/leave-request.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-request',
+  imports: [],
+  templateUrl: './leave-request.html',
+  styleUrl: './leave-request.css',
+})
+export class LeaveRequest {
+
+}
